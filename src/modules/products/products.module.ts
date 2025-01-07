7f70d92b4a4f3ababf3f65aaacb237931dceb237
Warllei <warllei.s.martins@hotmail.com>
2025-01-07 03:29:02 -0300

    correções

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { DbModule } from '../../db/db.module';
 import { ProductsRepository } from './products.repository';
-import { UserRepository } from '../user/user.repository';
 
 @Module({
   imports: [DbModule],
   controllers: [ProductsController],
-  providers: [ProductsRepository, UserRepository],
+  providers: [ProductsRepository],
 })
 export class ProductsModule { }
